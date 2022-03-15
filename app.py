6121c38cdba8935e528fa62dce921c53d12de588
MannyAbd <manal_abd@hotmail.com>
2022-03-14 20:21:06 -0400

    I can now search for specific drinks

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from crypt import methods
+from urllib import response
 from json2html import *
 from flask import Flask, request, redirect, render_template, flash
 import requests
@@ -7,23 +9,21 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'hi'
 
 api_key = 1
+# BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s={name}"
 BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php"
 
-# res = requests.get(f"{BASE_URL}", params={'api_key': api_key, 's': 'margarita'})
+def get_search(s):
+    res = requests.get(f"{BASE_URL}", params={'api_key': api_key, 's': s})
+    # res = requests.get(BASE_URL)
+    search = res.json()
+    name = search["drinks"][0]
+    return {"name": name}
+
 
 # res = requests.get("https://www.thecocktaildb.com/api/json/v1/1/search.php", params={'api_key': api_key, 's': 'margarita'})
-s = 'margarita'
-
-def get_search():
-  
-    res = requests.get(f"{BASE_URL}", params={'api_key': api_key, 's': s})
-    r = res.json()
-    return r
-    
-
+     ####################routes####################   
 @app.route('/')
 def homepage():
-
     return render_template('index.html')
 
 @app.route('/search')
@@ -31,8 +31,9 @@ def searched():
     #prints json into a html table
     # t = get_search()
     # search = json2html.convert(json=t)
-    search = get_search()
+    # term = request.args["drink"]
+    # search = get_search()
+    drink = request.args['name']
+    drink_name = get_search(drink)
 
-    return render_template('search-drink.html', search=search)
-    
-    
+    return render_template('index.html', drink_name=drink_name )
