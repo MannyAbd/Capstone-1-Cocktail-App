7756b14bbc862ccca0cb960fc0e87a882649f236
MannyAbd <manal_abd@hotmail.com>
2022-03-20 11:45:24 -0400

    can log in, out, and register

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from json2html import *
-from flask import Flask, request, redirect, render_template, flash
+
+from flask import Flask, request, redirect, render_template, flash, session
 import requests
+from models import connect_db, db, User, Ingredient, Filt, Drink, Favorite
+from forms import UserForm
+
+from sqlalchemy.exc import IntegrityError
+
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///capstone1"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_ECHO"] = True
 app.config['SECRET_KEY'] = 'hi'
+
+connect_db(app)
+
 api_key = 1
 BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php"
 
@@ -59,3 +69,51 @@ def searched_ingredient():
     drink = request.args['name']
     drink_ingredient = get_ingredient(drink)
     return render_template('search_drink.html', drink_ingredient=drink_ingredient)
+
+##############################login/register###############################
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_user():
+    form = UserForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        new_user = User.register(username, password)
+
+        db.session.add(new_user)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            form.username.errors.append('Username taken.  Please pick another')
+            return render_template('register.html', form=form)
+        session['user_id'] = new_user.id
+        flash('Welcome! Successfully Created Your Account!', "success")
+        return redirect('/')
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_user():
+    form = UserForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        user = User.authenticate(username, password)
+        if user:
+            flash(f"Welcome Back, {user.username}!", "primary")
+            session['user_id'] = user.id
+            return redirect('/')
+        else:
+            form.username.errors = ['Invalid username/password.']
+
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout_user():
+    session.pop('user_id')
+    flash("Goodbye!", "info")
+    return redirect('/')
+
+##############################loggedIn Route###############################
+
--- models.py
+++ models.py
@@ -1,6 +1,23 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+class Favorite(db.Model):
+    __tablename__ = "favorites"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    text = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    user = db.relationship('User', backref="favorites")
 
 class Drink(db.Model):
     __tablename__ = "drinks"
@@ -10,14 +27,14 @@ class Drink(db.Model):
     category = db.Column(db.Text, nullable=False)
     alcoholic = db.Column(db.Boolean)
     instructions= db.Column(db.Text, nullable=False)
-    thumbnail = db.Column(db.Image)
+    
     glass_type = db.Column(db.Text)
     ingredients_list = db.Column(db.Text, nullable=False)
     measurements_list = db.Column(db.Text, nullable=False)
 
-class Filter(db.Model):
+class Filt(db.Model):
     __tablename__ = "filters"
-
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     category = db.Column(db.Text, nullable=False)
     glass_type = db.Column(db.Text)
     ingredients_list = db.Column(db.Text, nullable=False)
@@ -31,9 +48,39 @@ class Ingredient(db.Model):
     description = db.Column(db.Text)
     alcoholic = db.Column(db.Boolean)
 
+class User(db.Model):
 
-def connect_db(app):
-    """Connect to database."""
+    __tablename__ = 'users'
 
-    db.app = app
-    db.init_app(app)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    username = db.Column(db.Text, nullable=False,  unique=True)
+
+    password = db.Column(db.Text, nullable=False)
+
+    
+    @classmethod
+    def register(cls, username, pwd):
+        """Register user w/hashed password & return user."""
+
+        hashed = bcrypt.generate_password_hash(pwd)
+        # turn bytestring into normal (unicode utf8) string
+        hashed_utf8 = hashed.decode("utf8")
+
+        # return instance of user w/username and hashed pwd
+        return cls(username=username, password=hashed_utf8)
+
+    @classmethod
+    def authenticate(cls, username, pwd):
+        """Validate that user exists & password is correct.
+
+        Return user if valid; else return False.
+        """
+
+        u = User.query.filter_by(username=username).first()
+
+        if u and bcrypt.check_password_hash(u.password, pwd):
+            # return user instance
+            return u
+        else:
+            return False
