6eb5b95abdb454b65681cefc3853aaec597d13e8
mannyabd <manal_abd@hotmail.com>
2022-03-29 15:00:30 -0400

    added css

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    'DATABASE_URL', 'postgres:///cocktail_db')
+    'DATABASE_URL', 'postgresql:///cocktail_db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'Temp23333')
