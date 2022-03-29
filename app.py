2778509c469afd1f9450133119c1402ae192d9a9
mannyabd <manal_abd@hotmail.com>
2022-03-29 19:51:48 -0400

    added postgres solve

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ if uri.startswith("postgres://"):
 # rest of connection code using the connection string `uri`
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    'DATABASE_URL', 'postgres:///cocktail_db')
+    'DATABASE_URL', 'postgresql:///cocktail_db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'Temp23333')
